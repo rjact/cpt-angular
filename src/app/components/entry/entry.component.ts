f3832e2ef1003f591634b7f6a022d8d0ea865c54
rjact <ray@rayacunto.com>
2018-06-18 17:04:14 -0400

    get ICD10 codes before you click the button

--- src/app/components/entry/entry.component.ts
+++ src/app/components/entry/entry.component.ts
@@ -14,7 +14,7 @@ import { Icd10Code } from '../../models/Icd10Code';
 export class EntryComponent implements OnInit {
 	public entry: Entry;
 	public newCode:string;
-	private activeCpt:string = '';
+	private activeCpt:CptCode;
 	private showIcd10List:boolean = false;
 	constructor(private dataService: DataService){}
     
@@ -26,7 +26,12 @@ export class EntryComponent implements OnInit {
 		if(this.newCode) {
 			this.dataService.getCptCode(this.newCode).subscribe(cpt => {
 				if(cpt.length > 0) {
+					let newCpt = new CptCode(cpt[0]);
 					this.entry.CptCodes.push(new CptCode(cpt[0]));
+					let icdSubscription = this.dataService.getIcd10Codes(cpt[0].CPTCode).subscribe(icd => {
+						this.entry.CptCodes.filter(o => o.CPTCode == newCpt.CPTCode).forEach(o => o.AllIcd10Codes = icd)
+						icdSubscription.unsubscribe();
+					})
 				}
 			});
 		}
@@ -36,7 +41,7 @@ export class EntryComponent implements OnInit {
 		this.showIcd10List = true;
 	}
 	addIcd10(icd10) {
-		const currCpt = this.entry.CptCodes.filter(c => c.CPTCode == this.activeCpt);
+		const currCpt = this.entry.CptCodes.filter(c => c.CPTCode == this.activeCpt.CPTCode);
 		currCpt[0].ICD10Codes.push(icd10);
 	}
 	removeIcd10(cpt:CptCode, code:string) {
--- src/app/components/icd10/icd10.component.ts
+++ src/app/components/icd10/icd10.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, OnDestroy, Output, EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy, OnChanges, Output, EventEmitter, SimpleChanges } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { Icd10Code } from '../../models/Icd10Code';
 import { ISubscription } from 'rxjs/Subscription';
@@ -8,11 +8,11 @@ import { ISubscription } from 'rxjs/Subscription';
     templateUrl: './icd10.component.html',
 	styleUrls: ['../../../assets/styles/icd10.css']
 })
-export class Icd10Component implements OnInit, OnDestroy {
-	private icd10codes:Array<Icd10Code>;
+export class Icd10Component implements OnInit, OnDestroy, OnChanges {
+	@Input() private icd10codes:Array<Icd10Code>;
 	private getIcd10Subscription:ISubscription;
 	private loading:boolean = true;
-	@Input() cpt:string;
+	//@Input() cpt:string;
 	@Output() onAddIcd10:EventEmitter<Icd10Code> = new EventEmitter<Icd10Code>();
 	@Output() onClose:EventEmitter<number> = new EventEmitter<number>();
 
@@ -27,14 +27,24 @@ export class Icd10Component implements OnInit, OnDestroy {
 	}
 	
 	ngOnInit() {
+		if(!this.icd10codes)
+			this.loading = true;
+		/*
 		this.getIcd10Subscription = this.dataService.getIcd10Codes(this.cpt).subscribe(codes => {
 			this.icd10codes = codes;
 			this.loading = false;
 		})
+		*/
 	}
 
 	ngOnDestroy() {
 		if(this.getIcd10Subscription) { this.getIcd10Subscription.unsubscribe(); }
+	}
+	ngOnChanges(changes:SimpleChanges) {
+		if(changes.icd10codes) {
+			this.icd10codes = changes.icd10codes.currentValue;
+			this.loading = false;
+		}
 	}
 
 }
